42aacf75a7552beef6b133a0b05a34e9124c9836
Vansh-123 <vg3369138@gmail.com>
2023-09-19 00:59:00 +0530

    code

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-qasda8gygoiqf2al.us.auth0.com"
+      clientId="vZzJduCrNfM3PCB4SjtVlUuKoskwST36"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
